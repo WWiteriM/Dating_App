5e0500ede2dfcc31a2c3a40d47f7748de29a8b0a
Arsenii <arsenii.mingazov@belstu.by>
2021-11-14 17:30:38 +0300

    Implemented register logic with initial form

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,14 +12,21 @@ export class NavComponent implements OnInit {
 
   model: any = {}
 
-  constructor() { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
 
   }
 
-  login(): void {
-    console.log(this.model);
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    })
   }
 
+  logout() {
+    this.accountService.logout();
+  }
 }
